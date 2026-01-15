acfe05e6974382d80d423bb6500b12a60dbaadd2
Saramoawad <saramoawad732@gmail.com>
2026-01-15 17:37:59 +0200

    edit loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,4 +1,4 @@
 function Loader() {
-    return <h1 style={{ color: '#fff', margin: '20px',width :'50%',margin:'auto'}}> movies are loading...<i className="fa-solid fa-spinner" style={{ color: '#fff' }}></i></h1>
+    return <h1 style={{ color: '#fff', width: '50%', margin: 'auto' }}> movies are loading...<i className="fa-solid fa-spinner" style={{ color: '#fff' }}></i></h1>
 }
 export default Loader;
